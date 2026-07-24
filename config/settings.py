25bae63528f1231ca0996bf2eb72c8092f9e175d
COD-Kartik <kartikmathur4205y@gmail.com>
2026-07-25 00:21:28 +0530

    Add STATIC_ROOT for Vercel static file collection

--- config/settings.py
+++ config/settings.py
@@ -145,6 +145,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
